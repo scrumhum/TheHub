0125b4d141c89efbc05213dfc2b8c12b16e6600f
Taro Oshiro <toshiro470@mycr.redwoods.edu>
2023-02-07 08:48:41 -0800

    Put in basic code to test my knowledge of where we are going.

--- Admin.java
+++ Admin.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Admin {
+
+    public Admin() {
+
+    }
+    List events = new ArrayList();
+    List venues = new ArrayList();
+
+    public void createEvent(Event event) {
+        events.add(event);
+    }
+
+    public void deleteEvent(Event event) {
+        events.remove(event);
+    }
+
+    public void createVenue(Venue venue) {
+        venues.add(venue);
+    }
+
+    public void deleteVenue(Venue venue) {
+        venues.remove(venue);
+    }
 }
---
+++ Event.java
@@ -0,0 +1,14 @@
+public class Event {
+
+    String eventTime;
+
+    Location eventLocation;
+
+    public String getEventTime() {
+        return eventTime;
+    }
+
+    public void setEventTime(String time) {
+        eventTime = time;
+    }
+}
