cd5128c6bf323cdfb1ecd9f17aa6d617668a88c4
scrumhum <makayla.kuhnke@gmail.com>
2023-02-28 11:42:03 -0800

    mak gathering manager

--- GatheringManager.java
+++ GatheringManager.java
@@ -1,29 +1,52 @@
-public class GatheringManager extends User{
+import java.util.ArrayList;
+import java.util.List;
 
-    /**
-     *
-     * Sudo Code:
-     *
-     * Constructor
-     *
-     * addGathering()
-     *
-     * removeGathering()
-     *
-     */
+    public class GatheringManager extends User {
+        private User currentUser;
+        private List<Gathering> gatherings;
 
-    public GatheringManager() {
+        public GatheringManager(String username, String password, String name, String email) {
+            super(username, password, name, email);
+            gatherings = new ArrayList<>();
+        }
 
+        public void login(String username, String password) {
+            if (this.getUserName().equals(username) && this.getPassword().equals(password)) {
+                currentUser = this;
+            }
+        }
+
+        public void editProfile(String name, String email, String password) {
+            currentUser.setFirstName(name);
+            currentUser.setEmail(email);
+            currentUser.setPassword(password);
+        }
+
+        public void addEventFromLink(String link) {
+            Event event = EventParser.parseFromLink(link);
+            if (event != null) {
+                currentUser.addEvent(event);
+            }
+        }
+
+        public void createEvent(String name, String photo, String description, String location, String time) {
+            Event event = new Event(name, photo, description, location, time);
+            currentUser.addEvent(event);
+        }
+
+        public void addGathering(Gathering gathering) {
+            gatherings.add(gathering);
+        }
+
+        public void removeGathering(Gathering gathering) {
+            gatherings.remove(gathering);
+        }
+
+        public List<Gathering> getGatherings() {
+            return gatherings;
+        }
+
+        public void setGatherings(List<Gathering> gatherings) {
+            this.gatherings = gatherings;
+        }
     }
-
-
-    /*
-    //TODO Need to create methods for setGathering & getGathering
-    public void addGathering() {
-        setGathering(getGathering().add(new Gathering()));
-    }
-
-    public void removeGathering(Gathering gathering) {
-        setGathering(getGathering().remove(gathering));
-    }*/
-}
