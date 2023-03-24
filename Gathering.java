5be8bc2e7992460da0acdb2447891a1e06317a6e
scrumhum <makayla.kuhnke@gmail.com>
2023-03-24 15:04:29 -0700

    updated gathering class to pull event data from database and push data to the calendar.

--- Gathering.java
+++ Gathering.java
@@ -1,49 +1,77 @@
-import javax.xml.stream.Location;
-import java.sql.Date;
-import java.sql.Time;
-import java.time.LocalDate;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 
-public class Gathering {
-    // This is meant to be a temporary event object and should be changed in the future
-    public Gathering(String gatheringName, Time gatheringTime, Date gatheringDate, Location gatheringLocation) {
-        this.gatheringName = gatheringName;
-        this.gatheringTime = gatheringTime;
-        this.gatheringDate = gatheringDate;
-        this.gatheringLocation = gatheringLocation;
+public class GatheringManager extends User {
+    private User currentUser;
+    private List<Gathering> gatherings;
+
+    public GatheringManager(String username, String password, String name, String email) {
+        super(username, password, name, email);
+        gatherings = new ArrayList<>();
     }
-    String gatheringName;
 
-    Time gatheringTime;
-
-    Date gatheringDate;
-
-    //User<User> volunteers = new ArrayList<User>();
-
-    Location gatheringLocation = new Location() {
-        @Override
-        public int getLineNumber() {
-            return 0;
+    public void login(String username, String password) {
+        if (this.getUsername().equals(username) && this.getPassword().equals(password)) {
+            currentUser = this;
         }
+    }
 
-        @Override
-        public int getColumnNumber() {
-            return 0;
-        }
+    public void editProfile(String name, String email, String password) {
+        currentUser.setName(name);
+        currentUser.setEmail(email);
+        currentUser.setPassword(password);
+    }
 
-        @Override
-        public int getCharacterOffset() {
-            return 0;
+    public void addEventFromLink(String link) {
+        Event event = EventParser.parseFromLink(link);
+        if (event != null) {
+            currentUser.addEvent(event);
         }
+    }
 
-        @Override
-        public String getPublicId() {
-            return null;
-        }
+    public void createEvent(String name, String photo, String description, String location, String time) {
+        Event event = new Event(name, photo, description, location, time);
+        currentUser.addEvent(event);
+    }
 
-        @Override
-        public String getSystemId() {
-            return null;
+    public void addGathering(Gathering gathering) {
+        gatherings.add(gathering);
+    }
+
+    public void removeGathering(Gathering gathering) {
+        gatherings.remove(gathering);
+    }
+
+    public List<Gathering> getGatherings() {
+        return gatherings;
+    }
+
+    public void setGatherings(List<Gathering> gatherings) {
+        this.gatherings = gatherings;
+    }
+
+    public void loadEventsFromDatabase(String url, String user, String password) {
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT name, photo, description, location, time FROM events")) {
+            while (rs.next()) {
+                String name = rs.getString("name");
+                String photo = rs.getString("photo");
+                String description = rs.getString("description");
+                String location = rs.getString("location");
+                String time = rs.getString("time");
+                Event event = new Event(name, photo, description, location, time);
+                currentUser.addEvent(event);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error loading events from database: " + e.getMessage());
         }
-    };
+    }
+
+    public void pushEventsToCalendar(Calendar calendar) {
+        for (Event event : currentUser.getEvents()) {
+            calendar.addEvent(event);
+        }
+    }
 }
